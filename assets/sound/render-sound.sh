b50cec05a52c6a2ee2c94237ebfd2a4494ab274e
detomon <simon@monoxid.ch>
2014-08-26 13:36:00 +0200

    Add sound render script

---
+++ assets/sound/render-sound.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+find . -type f -iname "*.blip" -print0 | while IFS= read -r -d $'\0' line; do
+	name=`echo "$line" | sed "s/\.blip$//g"`
+	bliplay --no-sound -o $name.wav $name.blip
+	ffmpeg -loglevel panic -y -i $name.wav -q:a 1 $name.mp3
+	unlink $name.wav
+done
