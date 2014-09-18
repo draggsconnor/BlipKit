484374705fd0bf589e029de8475a939df1d7ccbb
detomon <simon@monoxid.ch>
2014-09-18 14:39:52 +0200

    Update render script

--- assets/sound/render-sound.sh
+++ assets/sound/render-sound.sh
@@ -2,7 +2,10 @@
 
 find . -type f -iname "*.blip" -print0 | while IFS= read -r -d $'\0' line; do
 	name=`echo "$line" | sed "s/\.blip$//g"`
+	echo "Rendering $name..."
 	bliplay -m -o $name.wav $name.blip
 	ffmpeg -loglevel panic -y -i $name.wav -q:a 1 $name.mp3
 	unlink $name.wav
 done
+
+echo "Done"
