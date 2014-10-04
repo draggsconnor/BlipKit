6752a78595df8e6c58de0dcdbfbc5371e36c6c6c
detomon <simon@monoxid.ch>
2014-10-04 14:57:11 +0200

    Fix loop

--- assets/sound/render-sound.sh
+++ assets/sound/render-sound.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find . -type f -iname "*.blip" -print0 | while IFS= read -r -d $'\0' line; do
+for line in $(find . -iname '*.blip'); do
 	name=`echo "$line" | sed "s/\.blip$//g"`
 	echo "Rendering $name..."
 	bliplay -m -o $name.wav $name.blip
